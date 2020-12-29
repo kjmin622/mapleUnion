693e73cada0bf789d561eebc7a2485643af8409a
kjmin622 <kjmin622@naver.com>
2020-12-29 22:05:22 +0900

    color

--- js/UnionMap.js
+++ js/UnionMap.js
@@ -55,6 +55,41 @@ $(document).ready(function(){
             
         }
     }
-
     initMap();
+
+    const maps = Array.from(Array(20), () => Array(22).fill(false));
+    var mouseStatus = 0; // 0:non 1:add 2:delete
+    $('.unionBlock').mousedown(function(){
+        var id = $(this).attr('id').split('_');
+        var r = id[0];
+        var c = id[1];
+        if(maps[r][c]==false) mouseStatus = 1;
+        else mouseStatus = 2;
+        if(mouseStatus==1){
+            maps[r][c]=true;
+            $(this).css('background-color','rgb(150,150,150)');
+        }
+        if(mouseStatus==2){
+            maps[r][c]=false;
+            $(this).css('background-color','rgb(235,235,235)');
+        }
+    });
+    $('*').mouseup(function(){
+        mouseStatus = 0;
+    });
+    $('.unionBlock').mouseover(function(){
+        if(mouseStatus!=0){
+            var id = $(this).attr('id').split('_');
+            var r = id[0];
+            var c = id[1];
+            if(mouseStatus==1){
+                maps[r][c]=true;
+                $(this).css('background-color','rgb(150,150,150)');
+            }
+            if(mouseStatus==2){
+                maps[r][c]=false;
+                $(this).css('background-color','rgb(235,235,235)');
+            }
+        }
+    })
 });
